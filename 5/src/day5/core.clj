cd873a4b9fedee779a50d0d7e08f92ee98b32d40
kostspielig <kostspielig@gmail.com>
2018-12-06 02:56:47 +0100

    Day 5: Clojure - task2

--- 5/src/day5/core.clj
+++ 5/src/day5/core.clj
@@ -4,14 +4,14 @@
 
 
 ;; 65-90 (A-Z) 97-122 (a-z) = (+32)
-(defn same-char [c1 c2]
+(defn same-char-to-upper-or-lower [c1 c2]
   (if (or (nil? c1) (nil? c2))
     false
     (= (Math/abs (- c1 c2)) 32)))
 
 (defn task1 [ascii]
   (first (reduce (fn [[acc prev] i]
-                   (if (same-char prev i)
+                   (if (same-char-to-upper-or-lower prev i)
                      (if (= prev (last acc))
                        (let [next (pop acc)]
                          [next (last next)])
@@ -20,9 +20,31 @@
                  [[] nil]
                  ascii)))
 
+(defn same-char [c1 c2]
+  (if (or (nil? c1) (nil? c2))
+    false
+    (let [diff (Math/abs (- c1 c2))]
+      (or (= diff 32)
+        (= diff 0)))))
+
+(defn task2 [ascii]
+  (apply
+    min
+    (map (fn [n]
+           (count
+             (task1
+               (reduce
+                 (fn [acc i]
+                   (if (same-char i n)
+                     acc
+                     (conj acc i)))
+                 []
+                 ascii))))
+         (range 65 91))))
 
 (defn -main
   "Advent of code day 5."
   [& args]
   (let [input (str (read-string (slurp "input.txt")))]
-    (println (count (task1 (map int input))))))
+    (println (count (task1 (map int input))))
+    (println (task2 (map int input)))))
